e1043923a5a649b8bbb004d604c04c5b54aaad78
Per Arneng <per.arneng@scalebit.com>
2024-10-21 15:58:13 +0200

    some minor edits

--- src/main/java/com/scalebit/seevee/seeveecli/SeeVeeCliApplication.java
+++ src/main/java/com/scalebit/seevee/seeveecli/SeeVeeCliApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SeeVeeCliApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SeeVeeCliApplication.class, args);
 	}
